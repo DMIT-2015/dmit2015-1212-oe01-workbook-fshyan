a0ee621f143518b46e37d7fcb87219ec626482cf
fshyan <fshyan1@nait.ca>
2022-01-28 08:59:04 -0700

    AlbertaCovid19Case

---
+++ dmit2015-1212-oe01-demos-fshyan/src/main/java/ca/nait/dmit/domain/BMI.java
@@ -0,0 +1,39 @@
+package ca.nait.dmit.domain;
+
+public class BMI {
+
+    private double weight;
+    private double height;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public BMI() {
+    }
+
+    public BMI(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double bmi() {
+        return 0;
+    }
+
+    public String bmiCategory() {
+        return "";
+    }
+}
